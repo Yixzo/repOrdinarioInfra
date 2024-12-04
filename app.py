55e99ce2016d412fa2b1ce20346980fe3a5d900b
Yixzo <gordiuzumaki@gmail.com>
2024-12-04 17:18:52 -0600

    Hola

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/saludo', methods=['GET'])
 def saludo():
-    return jsonify({'mensaje': 'Hola Mundo'}), 200
+    return jsonify({'mensaje': 'Hola Mundo 2'}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
